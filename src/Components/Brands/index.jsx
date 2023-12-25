3848dd7aa23377d0c89f7fb4030386d0f5f0f394
HamidaAhmedqizi <hamidaahmedqizi@gmail.com>
2023-12-25 20:22:32 +0400

    add folder HomePage

--- src/Components/Brands/index.jsx
+++ src/Components/Brands/index.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import images from '../../Images/Vector.jpg'
+
+import images1 from '../../Images/Apple Podcast.png'
+import images2 from '../../Images/Spotify.png'
+import images3 from '../../Images/Sound Cloud.png'
+import images4 from '../../Images/Google Podcast.png'
+import './style.css'
+
 
 const Brands = () => {
+    const images=[images1,images2,images3, images4]
   return (
     <div className='containerBrands'>
            <div className='text'>Podcast Available On</div>
-           <div className="Apple">
-            <img src={images} alt="" />
+           <div className="Image">
+           {
+            images.map((image,index)=>{
+              return <article key={index}>
+           <img src={image} alt={`Gallery Image ${index}`} />
+              </article>
+            })
+          }
            </div>
     </div>
   )
