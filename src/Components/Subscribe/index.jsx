d779ec380b5d0d453e63a1684eb0e670d08a0b33
HamidaAhmedqizi <hamidaahmedqizi@gmail.com>
2023-12-28 00:53:35 +0400

    add folder

---
+++ src/Components/Subscribe/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './style.css'
+import image from '../../Images/Icon.png'
+
+const Subscribe = () => {
+    return (
+        <section className='Subscribe'>
+            <div className="Subscribe_container">
+                <div className="SubscribeLeft">
+                    <img src={image} alt="" />
+                </div>
+                <div className="SubscribeCenter">
+                    <h2>Receive new episodes in your inbox.</h2>
+                    <div className="input-button">
+                    <input type="text" placeholder='enter your Email' />
+                    <button className='button'>Subscribe'</button>
+                    </div>
+                </div>
+                <div className="SubscribeRight">
+                    <img src={image} alt="" />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Subscribe
