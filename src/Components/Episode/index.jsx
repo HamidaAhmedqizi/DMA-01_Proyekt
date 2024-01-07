eab91aecac54132bc84d65ee1e32075ae473713f
HamidaAhmedqizi <hamidaahmedqizi@gmail.com>
2024-01-07 16:40:26 +0400

    add folder

--- src/Components/Episode/index.jsx
+++ src/Components/Episode/index.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CardsData from '../../Data/CardsData'
 import EpisodeItem from '../EpisodeItem'
+import './style.css'
 
 const Episode = () => {
+  const [visible,setVisible]=useState(3);
+  const showMoreMeals = () => {
+   
+    setVisible((item) => (item += 6));
+  };
   return (
     <section className='episodes'>
       <div className='episodes_container'>
         <div className="episodes_top">
           <div className="episodes-top_container_left">
-            <h2 className='episodes-h2'>What our listeners say</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Mus cras lacus tellus morbi viverra
-              suspendisse ornare. Sit volutpat, volutpat ut
-              netus malesuada enim penatibus non aliquet.y
+            <h2 className='episodes-h2'>Recent Episodes</h2>
+            <p>Apparently we had reached a great height in the atmosphere, for
+              the sky was a dead black.
             </p>
           </div>
-
+          <div className="episodes_container-top_right">
+            <button onClick={() => showMoreMeals()} className="episodes_btn">
+              See All Episiodes
+            </button>
+          </div>
         </div>
         <div className="episodess_container-bottom">
 
@@ -24,7 +32,7 @@ const Episode = () => {
               
 
                 {
-                 CardsData.map(({id,image,  description, title})=>{
+                 CardsData.slice(0, visible).map(({id,image,  description, title})=>{
                     return (
                       <div key={id}>
                        <EpisodeItem id={id} image={image} title={title} description={description}/>
--- src/Components/EpisodeItem/index.jsx
+++ src/Components/EpisodeItem/index.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Episode_listen_icon from  '../../Images/Icon1.png'
 import Episode_icon from '../../Images/EpisodeIcon2.png'
+import './style.css'
 
 const EpisodeItem = ({ image,  description, title}) => {
     return (
-        <div className='episode_data_list'>
-            <div className='images_item'>
-               <img src={Episode_icon} alt="" />
+        <div className='episode_item_list'>
+            <div className='images_episodeitem'>
+               <img src={Episode_icon}className='episode_icon'alt="" />
                 <Link> <img src={image} alt="" /></Link>
             </div>
-            <div className="episode_data_text_icon">
-                <h3>{title}</h3>
-                <p>{description}</p>
-                <div className='episode_listen' >
+            <div className="episodeitem_text_icon">
+                <h3 className='episodeItem_title'>{title}</h3>
+                <p className='episodeItem_desc'>{description}</p>
+                <div className='episodeitem_listen' >
                 <img src={Episode_listen_icon} alt="" />
-                    <p className='listen text'> Listen now</p>
+                    <p className='listen_text'> Listen now</p>
                 </div>
             </div>
         </div>
